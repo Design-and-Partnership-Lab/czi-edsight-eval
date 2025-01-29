ab2db571d39aa2cfbc5e2387b5becb010267e302
Kevin Wu <kevinwu098@gmail.com>
2025-01-28 19:50:15 -0800

    chore: misc.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+import { foo } from "@/actions/foo/foo";
+
+export default async function Home() {
+    const bar = await foo();
+    console.log(bar);
+
     return (
         <div className="mx-auto flex w-full flex-col space-y-8 pt-16">home</div>
     );

af4761bd18aab3b9f96d5c8ca5bb3d16905da800
caleb vergene <calebverg@gmail.com>
2025-02-13 13:11:47 -0800

    fixed infinite loop POST request

--- src/components/reflection/ReflectionList.tsx
+++ src/components/reflection/ReflectionList.tsx
@@ -20,7 +20,7 @@ export default function ReflectionsList() {
 
     useEffect(() => {
         loadReflections();
-    }, [reflections]);
+    }, [reflections.length]);
 
     return (
         <div>

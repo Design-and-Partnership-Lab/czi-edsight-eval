26a2eaf61aa5f17f824c3dd4919b095416965654
Steven Lee <Stevel20@uci.edu>
2025-02-12 17:28:31 -0800

    feedback response fetch and add tasks

---
+++ src/actions/teacher/rr/reflectionresponse.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { db } from "@/db";
+import { UserFeedback_category } from "@prisma/client";
+
+interface upsertReflectionResponse {
+    studentId: number;
+    agree: Boolean;
+    reflectionId: string;
+    category: UserFeedback_category,
+    teacherEmail: string;
+}
+
+export async function upsertReflectionResponse({agree, reflectionId, category, teacherEmail }: upsertReflectionResponse) {
+    await db.userFeedback.upsert({
+        where: {
+            reflectionId_studentId_category_teacherEmail: {
+                studentId: 38695,
+                reflectionId: reflectionId,
+                category: category,
+                teacherEmail: teacherEmail,
+            },
+        },
+        create: {
+            studentId: 38695,
+            reflectionId: reflectionId,
+            agree: agree,
+            category: category,
+            teacherEmail: teacherEmail,
+        },
+        update: {
+            agree: agree,
+        },
+    });
+}

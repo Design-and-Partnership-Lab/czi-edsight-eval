4bdd664f1636d1d2757d0e15c1fae5463a1226e3
Caleb Vergene <150951123+calebvergene@users.noreply.github.com>
2025-02-25 10:07:11 -0800

    Authentication starter; Sign-up and Sign-in (#8)
    * auth redirecting works and start of sign in page
    
    * added design ui for login page
    
    * added white edsight logo svg format to login page
    
    * created login page
    
    * fixed sign up routing, made public
    
    ---------
    
    Co-authored-by: Aishwarya Thadiparthi <taishwarya@outlook.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
     return (
         <div className="mx-auto flex w-full flex-col space-y-8 pt-16">
             <SignedOut>
-                <SignInButton />
+                <SignInButton/>
             </SignedOut>
             <SignedIn>
                 <UserButton />

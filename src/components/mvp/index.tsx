e349ab0f6177b69d6da0c17f143ff0643fec1c29
Steven Lee <Stevel20@uci.edu>
2025-07-11 15:50:07 -0700

    task3 page updates with subsection ai rationale

--- src/components/mvp/index.tsx
+++ src/components/mvp/index.tsx
@@ -168,6 +168,7 @@ const MvpContent = ({
                         isReadOnly={true}
                     >
                         <TaskThree
+                            reflectionResponseId={reflectionResponseId}
                             aiEval="Excelling"
                             teacherEval={teacherEval}
                             handleCanProgress={handleCanProgress}

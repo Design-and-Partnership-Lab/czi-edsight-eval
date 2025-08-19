28acb6e7f41bcd67cbccd599aae391c48f753a2e
Gordon <GordonM04126@gmail.com>
2025-08-18 17:10:28 -0700

    Removed old index and redundant declaration

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+//interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+export const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(//, InputProps>(
   ({ className, ...props }, ref) => {
     return (
       <input
